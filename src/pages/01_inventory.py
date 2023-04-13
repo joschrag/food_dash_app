895398cc957c756f3eebb06ab413d41a3a7ea427
jonas.schrage <jonasschrage12@gmail.com>
2023-04-13 18:25:22 +0200

    Move style to css file (#2)

--- src/pages/01_inventory.py
+++ src/pages/01_inventory.py
@@ -13,42 +13,19 @@ dash.register_page(
 headline = html.H1("Inventory")
 
 
-# plus_btn_style = {
-#     "color": "white",
-#     "background-color": "#5fe515",
-#     "font-size": "32pt",
-#     "font-weight": "bold",
-#     "border-radius": "0.5em",
-#     "padding": "0.25em",
-#     "width": "auto",
-#     "aspect-ratio": "1 / 1",
-#     "text-align": "center",
-#     "vertical-align": "middle",
-#     "border": "none",
-# }
-
-# plus_button = html.Button("+", id="plus_button_inv", style=plus_btn_style)
-row_style = {"float": "left", "margin": "1em"}
-inp_style = {**row_style, "max-width": "10em"}
-btn_style = {
-    **inp_style,
-    "max-width": "10em",
-    "background": "#5bc0de",
-    "padding": "0.25em 0.5em",
-    "color": "black",
-    "border-radius": "0.5em",
-    "border": "none",
-}
-dd_style = {**inp_style, "margin-right": "3em"}
-name_txt = html.P("Item name:", style=row_style)
-amount_txt = html.P("number of items:", style=row_style)
+name_txt = html.P("Item name:", className="row-item")
+amount_txt = html.P("number of items:", className="row-item")
 cat_dd = dcc.Dropdown(
-    placeholder="Choose a category", searchable=True, style=dd_style
+    placeholder="Choose a category",
+    searchable=True,
+    className="row-item inv_dd",
 )
-amount_inp = dcc.Input(id="inv_amount", type="number", step=1, style=inp_style)
-name_inp = dcc.Input(id="inv_name", type="text", style=inp_style)
+amount_inp = dcc.Input(
+    id="inv_amount", type="number", step=1, className="row-input"
+)
+name_inp = dcc.Input(id="inv_name", type="text", className="row-input")
 submit = dcc.ConfirmDialogProvider(
-    children=html.Button("Submit", style=btn_style),
+    children=html.Button("Submit",className="row-item submit-btn"),
     id="inv_submit",
     message="Confirm your submission.",
 )
@@ -56,14 +33,6 @@ submit = dcc.ConfirmDialogProvider(
 add_item = html.Div(
     children=[name_txt, name_inp, cat_dd, amount_txt, amount_inp, submit],
     id="add_to_inventory",
-    style={
-        "min-witdh": "100px",
-        "width": "80em",
-        "min-height": "100px",
-        "padding": "1em",
-        "padding-left": "3em",
-        "display": "inline-block",
-    },
 )
 
 layout = dbc.Container([headline, add_item], fluid=True)
