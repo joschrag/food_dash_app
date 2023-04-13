9c0fad24757d0468c9582b89adb5d3c642b030e9
jonas.schrage <jonasschrage12@gmail.com>
2023-04-13 15:22:08 +0200

    Changing stylesheet and rewriting nav (#2)

--- src/app.py
+++ src/app.py
@@ -1,39 +1,46 @@
 import dash
-from dash import html
 import dash_bootstrap_components as dbc
+from dash import html
 
 # Define the Dash app
 app = dash.Dash(
     __name__,
     use_pages=True,
-    external_stylesheets=[
-        "https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/slate/bootstrap.min.css"
-    ],
+    external_stylesheets=[dbc.themes.SLATE],
 )
-
+nav_link_style = {
+    "margin": "1em 1em",
+    "text-align": "center",
+    "background": "#5bc0de",
+    "padding": "0.5em 2em",
+    "color": "black",
+}
 
 navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            # Use row and col to control vertical alignment of logo / brand
-            dbc.Row(
-                [
-                    dbc.Col(dbc.NavItem(dbc.NavLink("Home", href="/"))),
-                    dbc.Col(
-                        dbc.NavItem(
-                            dbc.NavLink("Inventory", href="./inventory")
-                        )
-                    ),
-                    dbc.Col(dbc.NavItem(dbc.NavLink("Meals", href="./meals"))),
-                ]
-            )
-            # Another column, etc.
-        ],
-        fluid=True,
-    ),
+    [
+        # Use row and col to control vertical alignment of logo / brand
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", style=nav_link_style),
+                dbc.NavLink(
+                    "Inventory", href="./inventory", style=nav_link_style
+                ),
+                dbc.NavLink("Meals", href="./meals", style=nav_link_style),
+            ],
+            vertical=False,
+            pills=True,
+        )
+        # Another column, etc.
+    ],
     color="dark",
     dark=True,
     sticky="top",
+    style={
+        "margin-left": "10em",
+        "margin-bottom": "3em",
+        "border": "none",
+        "width": "auto",
+    },
 )
 
 # Set the app layout
