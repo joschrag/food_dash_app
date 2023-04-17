0c7231123b1e204ab686ec586c4524c3a59744e2
jonas.schrage <jonasschrage12@gmail.com>
2023-04-17 21:21:38 +0200

    Overload __init__ and add arg for get_table (#4)

--- src/sql_handler.py
+++ src/sql_handler.py
@@ -18,17 +18,22 @@ class SQLHandler:
     def __init__(
         self,
         db_path: Path | str,
-        table: str,
+        table: str | None = None,
     ) -> None:
         """Initialize the class.
 
         Args:
             db_path (Path | str): path to db file
-            table (str): name of table
+            table (str): name of table, defaults to None
         """
         self.db_path = Path(db_path)
         self.table = table
-        self.establish_connection()
+        self.meta = sa.MetaData()
+
+        self.engine = sa.create_engine(f"sqlite:///{self.db_path}")
+        self.meta.reflect(self.engine, views=True)
+        if self.table is not None:
+            self.sqltable = self.get_table()
 
     def establish_connection(self) -> None:
         """Establish connection to a database."""
@@ -38,8 +43,11 @@ class SQLHandler:
         self.meta.reflect(self.engine, views=True)
         self.sqltable = self.get_table()
 
-    def get_table(self) -> sa.Table:
+    def get_table(self, table_name: str | None = None) -> sa.Table:
         """Get a table from the sqlite db.
+
+        Args:
+            table_name (str | None): name of table, defaults to None.
 
         Raises:
             KeyError: Table doesnt exist.
@@ -47,10 +55,10 @@ class SQLHandler:
         Returns:
             sa.Table: table from sqlite db
         """
-        if f"{self.table}" in self.meta.tables:
-            return self.meta.tables[f"{self.table}"]
-        else:
-            raise KeyError(f"Table {self.table} does not exist.")
+        table = table_name or self.table
+        if f"{table}" in self.meta.tables:
+            return self.meta.tables[f"{table}"]
+        raise KeyError(f"Table {table} does not exist.")
 
     def read_table(self) -> pd.DataFrame:
         """Read table or view from SQL server."""
